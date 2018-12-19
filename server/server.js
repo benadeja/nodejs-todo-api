66257b649a0392e4d09e14da4add626898490749
jbenade <jaco.benade@momentum.co.za>
2018-12-19 17:04:14 +0200

    Add delete resource

--- server/server.js
+++ server/server.js
@@ -42,10 +42,28 @@ app.get('/todos/:id', (req, res) => {
         if (!todo) {
             return res.status(404).send('ID not found');
         }
-        return res.send({todo});
+        return res.send({ todo });
     }).catch((e) => {
         res.status(400).send(e);
     })
+});
+
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('ID not valid');
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send('ID not found');
+        }
+
+        res.send(todo);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
 });
 
 app.listen(port, () => {
