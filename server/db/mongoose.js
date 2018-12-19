e7b3dcf75935bd8e9dfe46f4c6993b48e967865d
jbenade <jaco.benade@momentum.co.za>
2018-12-19 16:00:31 +0200

    Making db connection more dynamic

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,21 @@
+var os = require("os");
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
 const cloudMongo = 'mongodb://testuser1:testuser1@ds139534.mlab.com:39534/mongoose-for-nodejs-todo-api';
 const localMongo = 'mongodb://localhost:27017/TodoApp';
+var mongoHost = '';
 
-mongoose.connect(cloudMongo, { useMongoClient: true });
+if (os.hostname() === 'jaco-HP-EliteBook-850-G3') {
+    console.log('Connecting to local mongo host');
+    mongoHost = localMongo;
+}
+else {
+    console.log('Connecting to cloud mongo host');
+    mongoHost = cloudMongo
+}
+
+mongoose.connect(mongoHost, { useMongoClient: true });
 
 module.exports = { mongoose };
