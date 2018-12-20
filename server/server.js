cd1cae630a055d02dc31d35884aa5db738eb912d
jbenade <jaco.benade@momentum.co.za>
2018-12-20 11:07:19 +0200

    Add Test the delete todobyid route

--- server/server.js
+++ server/server.js
@@ -60,7 +60,7 @@ app.delete('/todos/:id', (req, res) => {
             return res.status(404).send('ID not found');
         }
 
-        res.send(todo);
+        res.send({todo});
     }).catch((e) => {
         res.status(400).send(e);
     });
